68b0ca6def45973bfc5c399acceb5d797c90e37d
Koen-Choy <64003030+Koen-Choy@users.noreply.github.com>
2020-05-03 19:32:26 +0800

    Week 2 Practical

--- MainActivity.java
+++ MainActivity.java
@@ -1,38 +1,133 @@
-package sg.edu.np.WhackAMole;
+package sg.edu.np.mad.WhackAMole;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
-
+import android.widget.TextView;
+import android.widget.Button;
+import android.view.View;
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
 
-    /* Hint
-        - The function setNewMole() uses the Random class to generate a random value ranged from 0 to 2.
-        - Feel free to modify the function to suit your program.
-    */
 
+public class MainActivity extends AppCompatActivity
+{
+
+    private static final String TAG ="Whack-A-Mole!";
+    private Button middle;
+    private Button right;
+    private Button left;
+    private String mole;
+    private Button target;
+    private int score;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        left = findViewById(R.id.first);
+        middle = findViewById(R.id.second);
+        right = findViewById(R.id.third);
 
-        Log.v(TAG, "Finished Pre-Initialisation!");
+        left.setOnClickListener(buttonOnClickListener);
+        middle.setOnClickListener(buttonOnClickListener);
+        right.setOnClickListener(buttonOnClickListener);
+
+        Log.v(TAG, "Loading Complete!");
+
     }
 
     @Override
-    protected void onStart(){
-        super.onStart();
-        setNewMole();
-        Log.v(TAG, "Starting GUI!");
-    }
-
-
-    public void setNewMole()
+    protected void onStart()
     {
-        Random ran = new Random();
-        int randomLocation = ran.nextInt(3);
+        super.onStart();
+        setGame();
+        Log.v(TAG, "Starting Game...");
     }
+
+    public void setGame()
+    {
+        Random r = new Random();
+        int rLocation = r.nextInt(3);
+        switch (mole)
+        {
+            case "first":{
+                left.setText("*");
+                middle.setText("0");
+                right.setText("0");
+                break;
+            }
+            case "second":{
+                left.setText("0");
+                middle.setText("*");
+                right.setText("0");
+                break;
+            }
+            default:{
+                left.setText("0");
+                middle.setText("0");
+                right.setText("*");
+                break;
+            }
+
+        }
+    }
+
+    private View.OnClickListener buttonOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            TextView textView = (TextView) findViewById(R.id.textView);
+            switch (v.getId())
+            {
+                case R.id.first:
+                    {
+                    if (left.getText() == "*")
+                    {
+                        score += 1;
+                        Log.d(TAG, "Hit, score added!");
+                    }
+                    else {
+                        score -= 1;
+                        Log.d(TAG, "Missed, score deducted!");
+                    }
+                    Log.d(TAG, "Left button is clicked!");
+                    textView.setText("Score is: " + score);
+                    setGame();
+                    break;
+                }
+                case R.id.second:
+                    {
+                    if (middle.getText() == "*")
+                    {
+                        score += 1;
+                        Log.d(TAG, "Hit, score added!");
+                    }
+                    else {
+                        score -= 1;
+                        Log.d(TAG, "Missed, score deducted!");
+                    }
+                    Log.d(TAG, "Middle button is clicked!");
+                    textView.setText("Score is: " + score);
+                    setGame();
+                    break;
+                }
+                default:
+                    {
+                    if (right.getText() == "*")
+                    {
+                        score += 1;
+                        Log.d(TAG, "Hit, score added!");
+                    }
+                    else {
+                        score -= 1;
+                        Log.d(TAG, "Missed, score deducted!");
+                    }
+                    Log.d(TAG, "Right button is clicked!");
+                    textView.setText("Score is: " + score);
+                    setGame();
+                    break;
+                }
+            }
+        }
+    };
 }
